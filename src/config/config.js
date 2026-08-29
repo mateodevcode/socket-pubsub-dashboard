e2b98c3db03a42acc4f7b925c21e10e72d36a0b7
Mateo lizcano <mateodevcode@gmail.com>
2026-08-30 00:10:34 +0200

    Se actualizo las env

--- src/config/config.js
+++ src/config/config.js
@@ -1,3 +1,5 @@
 export const VITE_CORE_REST_URL = import.meta.env.VITE_CORE_REST_URL;
 export const VITE_CORE_WS_URL = import.meta.env.VITE_CORE_WS_URL;
 export const VITE_API_URL = import.meta.env.VITE_API_URL;
+
+
