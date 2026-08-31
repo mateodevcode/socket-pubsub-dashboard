f51961955485495cf805360d39ad6780a539416d
Mateo lizcano <mateodevcode@gmail.com>
2026-08-31 10:18:35 +0200

    se comento docker

--- src/pages/AdminPanel.jsx
+++ src/pages/AdminPanel.jsx
@@ -142,19 +142,19 @@ export default function AdminPanel() {
           if (action === "disk_space") {
             setDiskSpace(parseDashboardData(output));
           }
-          if (action === "docker_info") {
-            console.log(data);
-            setDockerInfo({
-              containers: data.full_state?.containers || [],
-              delta: data.delta || {
-                added: [],
-                removed: [],
-                changed: [],
-              },
-              timestamp: data.timestamp,
-              success: data.success,
-            });
-          }
+          // if (action === "docker_info") {
+          //   console.log(data);
+          //   setDockerInfo({
+          //     containers: data.full_state?.containers || [],
+          //     delta: data.delta || {
+          //       added: [],
+          //       removed: [],
+          //       changed: [],
+          //     },
+          //     timestamp: data.timestamp,
+          //     success: data.success,
+          //   });
+          // }
 
           if (action === "uptime_check") {
             setUptimeCheck(parseDashboardData(output));
